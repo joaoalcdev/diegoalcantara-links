7e18bee91eb0a5af2f7817fb494549de0ff5b7d3
João Alcântara <joaosantalcantara@gmail.com>
2021-07-19 06:15:44 -0300

    update landing

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,24 @@ export default function Home() {
         ></link>
         {/* url/title contents */}
         <title>Diego Alcântara | Links</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favico/favicon-32x32.png"
+        ></link>
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <link rel="manifest" href="/site.webmanifest"></link>
         {/* top url color mobile ↓ */}
         <meta
           name="theme-color"
@@ -87,7 +105,6 @@ export default function Home() {
               na redação <br /> do ENEM
             </span>
           </paragraph>
-
           {/* social media */}
           <ul class="link-grid">
             {/* social card 1 */}
@@ -162,6 +179,51 @@ export default function Home() {
             </li>
           </ul>
 
+          {/* assets background */}
+          <svg
+            width="24"
+            height="24"
+            class="object__img01"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 35 35"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M10.999 4.442v-.71c-.598-.346-1-.992-1-1.732 0-1.104.896-2 2-2s2 .896 2 2c0 .74-.402 1.386-1 1.732v.71c.144-.023.268-.053.36-.089 1.206-.459 1.872-2.286 3.948-2.353 2.21-.029 3.418 2.251 5.173 2.769.473.139 1.027.142 1.52.172-.635 4.108-6.593 5.872-11.001 4.725v6.923c.843.262 1.911.611 2.542 1.201 2.008 1.877.83 4.698-1.539 4.787v-1c1.266-.627 1.504-1.961-1.003-2.878v4.301c0 .553-.448 1-1 1s-1-.447-1-1v-4.896c-1.955-.51-5.364-1.17-5.339-4.003.018-1.696 1.55-3.101 3.331-3.101h.01c.553 0 1 .448 1 1 0 .549-.442.994-.989 1-.836.009-1.392.651-1.349 1.192.098 1.124 2.22 1.556 3.336 1.844v-6.369c-4.407 1.146-10.364-.619-10.999-4.726.493-.03 1.047-.033 1.52-.172 1.755-.518 2.963-2.798 5.173-2.769 2.076.067 2.742 1.894 3.948 2.353.092.036.215.065.358.089zm-3.039 13.923c.863.25 1.655.463 2.35.624-1.691.869-1.495 2.002-.311 2.588v1c-2.138-.08-3.305-2.384-2.039-4.212zm6.073-2.606c.889-.257 2.232-.644 2.305-1.567.043-.543-.516-1.183-1.35-1.192-.547-.006-.989-.451-.989-1 0-.552.448-1 1-1h.011c1.772 0 3.313 1.399 3.33 3.101.017 1.273-.851 2.367-1.986 2.934-.6-.505-1.397-.909-2.321-1.276zm5.773-8.647c.512-.24.961-.534 1.308-.886-1.406-.386-2.573-2.218-3.767-2.227-1.241.04-2.034 2.353-4.347 2.461v1.193c.605.22 1.527.336 2.551.323-.124-.484-.177-1.003-.186-1.605.413.611.955 1.129 1.523 1.516.478-.057.958-.142 1.422-.257-.394-.587-.6-1.203-.729-1.879.645.66 1.413 1.068 2.225 1.361zm-8.807-.652c-2.312-.108-3.106-2.421-4.346-2.461-1.194.009-2.361 1.841-3.767 2.227.347.352.796.646 1.308.886.812-.293 1.58-.701 2.225-1.361-.129.676-.335 1.292-.729 1.879.464.115.944.2 1.422.257.568-.387 1.11-.905 1.523-1.516-.009.602-.062 1.121-.186 1.605 1.023.013 1.945-.103 2.55-.322v-1.194z" />
+          </svg>
+
+          <svg
+            width="24"
+            height="24"
+            class="object__img02"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 35 35"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M23 5v13.883l-1 .117v-16c-3.895.119-7.505.762-10.002 2.316-2.496-1.554-6.102-2.197-9.998-2.316v16l-1-.117v-13.883h-1v15h9.057c1.479 0 1.641 1 2.941 1 1.304 0 1.461-1 2.942-1h9.06v-15h-1zm-12 13.645c-1.946-.772-4.137-1.269-7-1.484v-12.051c2.352.197 4.996.675 7 1.922v11.613zm9-1.484c-2.863.215-5.054.712-7 1.484v-11.613c2.004-1.247 4.648-1.725 7-1.922v12.051z" />
+          </svg>
+
+          <svg
+            width="24"
+            height="24"
+            class="object__img03"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 35 35"
+            fill-rule="evenodd"
+            clip-rule="evenodd"
+          >
+            <path d="M22 6c1.104 0 2 .896 2 2v12c0 1.104-.896 2-2 2h-20c-1.104 0-2-.896-2-2v-12c0-1.104.896-2 2-2h5v-2c0-1.104.896-2 2-2h6c1.104 0 2 .896 2 2v2h5zm0 2.5c0-.276-.224-.5-.5-.5h-19c-.276 0-.5.224-.5.5v11c0 .276.224.5.5.5h19c.276 0 .5-.224.5-.5v-11zm-9 4.5h3v2h-3v3h-2v-3h-3v-2h3v-3h2v3zm1.5-9h-5c-.276 0-.5.224-.5.5v1.5h6v-1.5c0-.276-.224-.5-.5-.5" />
+          </svg>
+
+          <div>
+            {/* <div class="object__dotted04"></div> */}
+            <div class="object__dotted03"></div>
+            <div class="object__dotted02"></div>
+            <div class="object__dotted01"></div>
+          </div>
+          {/* assets background */}
+
           {/* video section card */}
           <h2 class="heading heading-highlight">Conteúdos</h2>
           <article class="card resource">
@@ -234,7 +296,7 @@ export default function Home() {
                 Cronograma
               </h3>
               <p>
-                Meu cronograma que usei para estudo e rotino, e, que me ajudou a
+                Meu cronograma de estudo e rotina que, me ajudou a
                 passar para medicina
                 <span class="highlight">.</span>
               </p>
